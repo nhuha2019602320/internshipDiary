a85410969787a4e8aa64295764405ee7eed102ad
unknown <nhuhai17072106@gmail.com>
2023-01-20 22:44:15 +0700

    updateWeek2

---
+++ Week2/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import ListTodo from './Todos/ListTodo.js'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ListTodo />
+      {/* <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      /> */}
+    </div>
+  );
+}
+
+export default App;
